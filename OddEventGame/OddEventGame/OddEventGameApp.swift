e222251a38f0ff4bc975e40070c4d8efcd60acdd
golder1023j <golder1023j@gmail.com>
2025-03-24 22:48:22 +0900

    create new project

---
+++ OddEventGame/OddEventGame/OddEventGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  OddEventGameApp.swift
+//  OddEventGame
+//
+//  Created by Chris on 2025/03/24.
+//
+
+import SwiftUI
+
+@main
+struct OddEventGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
