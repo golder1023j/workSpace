72446f39c3ad477f5f5878f6b62481a8b9883b56
golder1023j <golder1023j@gmail.com>
2025-03-24 22:52:33 +0900

    create new project

---
+++ OddOrEvenGame/OddOrEvenGame/OddOrEvenGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  OddOrEvenGameApp.swift
+//  OddOrEvenGame
+//
+//  Created by Chris on 2025/03/24.
+//
+
+import SwiftUI
+
+@main
+struct OddOrEvenGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
